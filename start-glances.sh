4ffad484dabe32717549f1f3bc92aebbb51e3486
DeusXIII <sylvain.jacouty@gmail.com>
2016-12-01 13:19:40 +0100

    Update start-glances.sh

--- start-glances.sh
+++ start-glances.sh
@@ -4,7 +4,11 @@
 docker run -d \
   --name glances \
   -e GLANCES_OPT="-w" \
+  -v "$PWD"/glances.conf:/glances/conf/glances.conf:ro \
   -v /var/run/docker.sock:/var/run/docker.sock:ro \
+  -v /mnt:/mnt:ro \
   -p 61208:61208 \
   --pid host \
+  --privileged \
+  --network="host" \
   sjacouty/docker-glances
